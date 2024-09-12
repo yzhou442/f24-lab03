5ed40f23afb10e92b614354c99571488c454968d
yzhou442 <yzhou020404@gmail.com>
2024-09-12 18:23:53 -0400

    Add implementation for DelegationSortedIntList in java

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,54 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList{
     // Write your implementation below with API documentation
+    private SortedIntList list;
+    private int totalAdded = 0;
+    
+    DelegationSortedIntList(){
+        list = new SortedIntList();
+        totalAdded = 0;
+    }
 
+    DelegationSortedIntList(SortedIntList _list){
+        list = _list;
+        totalAdded = list.size();
+    }
+
+    public int getTotalAdded(){
+        return totalAdded;
+    }
+
+    @Override
+    public boolean add(int num) {
+        totalAdded++;
+        return list.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList _list) {
+        totalAdded+=_list.size();
+        return list.addAll(_list);
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public int get(int index) {
+        return list.get(index); 
+    }
+
+    @Override
+    public boolean remove(int num){
+        return list.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList _list){
+        return list.removeAll(_list);
+    }
 }
