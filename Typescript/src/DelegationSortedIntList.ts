88abc62768e654efbcc49bfe3d2f3c4805bfff77
yzhou442 <yzhou020404@gmail.com>
2024-09-16 14:53:04 -0400

    Add implementation for DelegationSortedIntList.ts, pass all test cases

--- Typescript/src/DelegationSortedIntList.ts
+++ Typescript/src/DelegationSortedIntList.ts
@@ -7,9 +7,53 @@
  * @author Nora Shoemaker
  *
  */
-
-class DelegationSortedIntList {
+import { SortedIntList } from "./hidden/SortedIntListLibrary";
+class DelegationSortedIntList extends SortedIntList {
   // Write your implementation below with API documentatioin
+  private totalAdded: number = 0;
+  private list: SortedIntList;
+
+  constructor();
+  constructor(_list: SortedIntList);
+
+  constructor(_list?: SortedIntList) {
+    super();
+    this.list = _list || new SortedIntList();
+    this.totalAdded = 0;
+  }
+
+  add(num: number): boolean {
+    this.totalAdded++;
+    return this.list.add(num);
+  }
+
+  getTotalAdded(): number {
+    return this.totalAdded;
+  }
+
+  addAll(list: SortedIntList): boolean {
+    this.totalAdded += list.size();
+    return this.list.addAll(list);
+  }
+
+  get(index: number): number {
+    return this.list.get(index);
+  }
+
+  size(): number {
+    return this.list.size();
+  }
+
+  // remove(num: number): boolean {
+  //   return this.list.remove(num);
+  // }
+
+  // removeAll(list: SortedIntList): boolean {
+  //   return this.list.removeAll(list);
+  // }
+
+
+
 }
 
 export { DelegationSortedIntList }
